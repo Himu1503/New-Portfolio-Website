724dc901e0bc12486ec52328207a509519921f05
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2022-04-25 18:11:54 +0100

    end

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    title: 'Earthquake Detection using Machine Learning ',
+    description: "",
       image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
+      tags: ['Machine Learning', 'R', '' ],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
